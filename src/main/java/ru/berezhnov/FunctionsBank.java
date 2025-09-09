7f3417f6f7d42bbbb4fbd359631425c11b4fdaad
Aleksey Berezhnov <lstmidnight@yadex.ru>
2025-09-09 10:07:37 +0600

    Добавлен новый класс, в котором хранятся данные о функциях

---
+++ src/main/java/ru/berezhnov/FunctionsBank.java
@@ -0,0 +1,20 @@
+package main.java.ru.berezhnov;
+
+import java.util.Map;
+import java.util.function.Function;
+
+public class FunctionsBank {
+    private static Map<String, Function<double[], Function<Double, Double>>> functionBank = Map.of(
+            "sin(n*x)", arr -> x -> Math.sin(arr[0] * x),
+            "a*cos(n*x)", arr -> x -> arr[0] * Math.cos(arr[1] * x),
+            "weirdFunc", arr -> x -> Math.sin(x) * Math.tanh(x) + Math.cos(3*x) * 0.5
+    );
+
+    public static Function<Double, Double> getFunctionByName(String name, double... params) {
+        Function<double[], Function<Double, Double>> funcGenerator = functionBank.get(name);
+        if (funcGenerator == null) {
+            throw new IllegalArgumentException("Функция с именем '" + name + "' не найдена!");
+        }
+        return funcGenerator.apply(params);
+    }
+}
